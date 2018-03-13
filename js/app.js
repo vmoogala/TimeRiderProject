9c113bcbdded94b4afcf9db58c37de145b046db8
Vamshi Krishna Moogala <v.m@dal.ca>
2018-03-13 01:15:44 -0300

    Layout changes and added components

--- js/app.js
+++ js/app.js
@@ -7,3 +7,19 @@ d3.csv("/data/TimeRider-Data.csv", function(data) {
 });
 
 d3.select("#tooltip1").style("visibility", "hidden")
+
+
+var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+
+var svg = d3.select("#svg-div")
+    .append("svg")
+    .attr("width", "900")
+    .attr("height","500");
