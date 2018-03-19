e539e2287bf6ac5cb97e70aaf0d7454acfce9731
Vamshi Krishna Moogala <v.m@dal.ca>
2018-03-19 02:46:03 -0300

    Adding range boxes in graph

--- js/app.js
+++ js/app.js
@@ -7,42 +7,80 @@
 //     output.innerHTML = this.value;
 // }
 
-function changeThresholdValue(){
+function changeThresholdValue() {
 	// console.log("entered onblur");
 	var x = document.getElementById("input-y-axis-upper-threshold");
 	console.log(x.value);
 	// console.log(d3.select('#input-y-axis-upper-threshold')[0].value);
 }
 
-function yAxisRangeChecked(){
+
+function yAxisRangeChecked() {
 	console.log("entered yAxisRangeChecked");
 	var x = document.getElementById("y-axis-range-checkbox");
-	if(x.checked){
+	if (x.checked) {
 		console.log("show divs");
-	}else{
+		showRangesForYaxis(0, 0);
+	} else {
 		console.log("hide divs");
 	}
 }
 
 
-function getHtmlStringForToolTip(d){
+function showRangesForYaxis(minY, maxY) {
+	console.log("entered showRangesForYaxis");
+	var yAxisRangeBox = svg.append("g")
+	yAxisRangeBox.append("rect")
+		.attr("x", function() {
+			return xScale(0);
+		})
+		.attr("y", function() {
+			return yScale(maxY);
+		})
+		.attr("width", xScale(100))
+		.attr("height", function() {
+			return yScale(minY) - yScale(maxY);
+		})
+		.attr("class", "y-axis-range-box")
+}
+
+
+function showRangesForXaxis(minX, maxX) {
+	console.log("entered showRangesForXaxis");
+	var xAxisRangeBox = svg.append("g")
+	xAxisRangeBox.append("rect")
+		.attr("y", function() {
+			return yScale(100);
+		})
+		.attr("x", function() {
+			return xScale(minX);
+		})
+		.attr("height", yScale(0))
+		.attr("width", function() {
+			return xScale(maxX) - xScale(minX);
+		})
+		.attr("class", "x-axis-range-box")
+}
+
+
+function getHtmlStringForToolTip(d) {
 	var html = '<div class="heading-tooltip"><p>Date: <b>' + d.admission_date +
-	'</b><br/><b>' + d.patient_id + ': ' + d.patient_name +
-	'</b>, ' + d.gender + 
-	'<br/>Date of birth: ' + d.date_of_birth + 
-	'</p></div><div class="left-tooltip"><div><span class="header-text-tooltip">Biometric data</span><div class ="key-names-tooltip"> Body Mass Index <br/>Size[cm] <br/>Weight[kg] <br/>BP syst. [mmHg] <br/>BP diast. [mmHg] <br/>Smoker <br/></div><div class ="value-names-tooltip">' + 
-	d.bmi + ' <br/>' + d.size+' <br/>' + d.weight + '<br/>' + d.bp_syst + '<br/>' + d.bp_diast + '<br/>' + d.smoker + 
-	'<br/></div></div><div><span class="header-text-tooltip">OAD therapy</span><div class ="key-names-tooltip">SH <br/>Met <br/>Glit <br/>DPP4 <br/>Acarb <br/></div><div class ="value-names-tooltip">' +  
-	d.sh + ' <br/>' + d.met +' <br/>' +d.glit  + '<br/>' + d.dpp4  + '<br/>' +d.acarb  + '<br/>' + 
-	'</div></div><div><span class="header-text-tooltip">Concomitant medication</span><div class ="key-names-tooltip">ACE <br/>Bbl <br/>BP Other <br/>Statin <br/>ASS <br/></div><div class ="value-names-tooltip">' + 
-	d.ace  + ' <br/>' + d.bbl +' <br/>' +d.bp_other  + '<br/>' + d.statin  + '<br/>' +d.ass  + '<br/>' + 
-	'</div></div></div><div class="right-tooltip"><div><span class="header-text-tooltip">Laboratory</span><div class ="key-names-tooltip">Blood sugar [mg/dl] <br/>HbA1c[%] <br/>Cholesterol [mg/dl] <br/>Triglyceride [mg/dl] <br/>Creatinine [mg/dl] <br/>Protein in urine <br/></div><div class ="value-names-tooltip">' + 
-	d.blood_sugar  + ' <br/>' +d.hba1c +' <br/>' + d.cholesterol  + '<br/>' +d.triglyceride  + '<br/>' + d.creatinine  + '<br/>' + d.protein_in_urine + 
-	'<br/></div></div><div><span class="header-text-tooltip">Insulin therapy</span><div class ="key-names-tooltip">VZI <br/>ALT <br/>Misch <br/></div><div class ="value-names-tooltip">' + 
-	d.vzi + '<br/>' + d.alt + '<br/>' + d.misch + '<br/>' + 
-	'</div></div><div><span class="header-text-tooltip">Organ Damages</span><div class ="key-names-tooltip">Retinopathy <br/>Stroke <br/>Coronary Heart D. <br/>PAOD <br/>Polyneuropathy <br/>Nephropathie <br/></div><div class ="value-names-tooltip">' + 
-	d.retinopathy + ' <br/>' + d.stroke+' <br/>' + d.coronary_heart_d + '<br/>' + d.paod + '<br/>' + d.polyneuropathy + '<br/>' + d.nephropathie + 
-	'<br/></div></div></div>';
+		'</b><br/><b>' + d.patient_id + ': ' + d.patient_name +
+		'</b>, ' + d.gender +
+		'<br/>Date of birth: ' + d.date_of_birth +
+		'</p></div><div class="left-tooltip"><div><span class="header-text-tooltip">Biometric data</span><div class ="key-names-tooltip"> Body Mass Index <br/>Size[cm] <br/>Weight[kg] <br/>BP syst. [mmHg] <br/>BP diast. [mmHg] <br/>Smoker <br/></div><div class ="value-names-tooltip">' +
+		d.bmi + ' <br/>' + d.size + ' <br/>' + d.weight + '<br/>' + d.bp_syst + '<br/>' + d.bp_diast + '<br/>' + d.smoker +
+		'<br/></div></div><div><span class="header-text-tooltip">OAD therapy</span><div class ="key-names-tooltip">SH <br/>Met <br/>Glit <br/>DPP4 <br/>Acarb <br/></div><div class ="value-names-tooltip">' +
+		d.sh + ' <br/>' + d.met + ' <br/>' + d.glit + '<br/>' + d.dpp4 + '<br/>' + d.acarb + '<br/>' +
+		'</div></div><div><span class="header-text-tooltip">Concomitant medication</span><div class ="key-names-tooltip">ACE <br/>Bbl <br/>BP Other <br/>Statin <br/>ASS <br/></div><div class ="value-names-tooltip">' +
+		d.ace + ' <br/>' + d.bbl + ' <br/>' + d.bp_other + '<br/>' + d.statin + '<br/>' + d.ass + '<br/>' +
+		'</div></div></div><div class="right-tooltip"><div><span class="header-text-tooltip">Laboratory</span><div class ="key-names-tooltip">Blood sugar [mg/dl] <br/>HbA1c[%] <br/>Cholesterol [mg/dl] <br/>Triglyceride [mg/dl] <br/>Creatinine [mg/dl] <br/>Protein in urine <br/></div><div class ="value-names-tooltip">' +
+		d.blood_sugar + ' <br/>' + d.hba1c + ' <br/>' + d.cholesterol + '<br/>' + d.triglyceride + '<br/>' + d.creatinine + '<br/>' + d.protein_in_urine +
+		'<br/></div></div><div><span class="header-text-tooltip">Insulin therapy</span><div class ="key-names-tooltip">VZI <br/>ALT <br/>Misch <br/></div><div class ="value-names-tooltip">' +
+		d.vzi + '<br/>' + d.alt + '<br/>' + d.misch + '<br/>' +
+		'</div></div><div><span class="header-text-tooltip">Organ Damages</span><div class ="key-names-tooltip">Retinopathy <br/>Stroke <br/>Coronary Heart D. <br/>PAOD <br/>Polyneuropathy <br/>Nephropathie <br/></div><div class ="value-names-tooltip">' +
+		d.retinopathy + ' <br/>' + d.stroke + ' <br/>' + d.coronary_heart_d + '<br/>' + d.paod + '<br/>' + d.polyneuropathy + '<br/>' + d.nephropathie +
+		'<br/></div></div></div>';
 	return html;
 }
 
@@ -134,65 +172,51 @@ var tooltip = d3.select("body").append("div")
 	.attr("class", "tooltip1")
 	.attr("id", "tooltip1")
 	.style("opacity", 0);
-	// .style("display", "none");
+// .style("display", "none");
 
 
 // Scatter points
-function drawChart(data){
+function drawChart(data) {
 	chartProperties.data = data;
 	console.log(chartProperties.data);
 	svg.selectAll("circle")
-	.data(chartProperties.data)
-	.enter()
-	.append("circle")
-	.attr("class", "dot")
-	.attr("r", 5)
-	.attr("cx", function(d) {
-		// if(d.bmi == Nan){
-		// 	d.bmi = 0;
-		// }
-		console.log(+d.bmi*50);
-		return xScale(+d.bmi*10);
-	})
-	.attr("cy", function(d) {
-		return yScale(+d.hba1c*10);
-	})
-	// svg.append("circle")
-	// .attr("r", 5)
-	// .attr("cx", 700)
-	// .attr("cy", 90)
-	.on("mouseover", function(d){
-		// console.log(d);
-		mouseover(d);
-	})
-	.on("mouseout", mouseout);
-}
-
-function mouseover(d) {
-	// console.log("mouseover");
-	// tooltip.style("display", "inline");
-	tooltip.html(getHtmlStringForToolTip(d))
-		.style("left", (d3.event.pageX + 3) + "px")
-		.style("top", (d3.event.pageY + 3) + "px")
-		// .style("display", "inline")
-		.transition()
-		.duration(200) // ms
-		.style("opacity", .9);
-
-}
-
-// function mousemove() {
-// 	console.log("mousemove");
-// 	// tooltip.text(d3.event.pageX + ", " + d3.event.pageY)
-// 	// 	.style("left", (d3.event.pageX - 34) + "px")
-// 	// 	.style("top", (d3.event.pageY - 12) + "px");
-// }
-
-function mouseout() {
-	// console.log("mouseout");
-	// tooltip.style("display", "none")
-	tooltip
-	.transition()
-	.duration(200) // ms
-	.style("opacity", 0);;
+		.data(chartProperties.data)
+		.enter()
+		.append("circle")
+		.attr("class", "dot")
+		.attr("r", 5)
+		.attr("cx", function(d) {
+			// if(d.bmi == Nan){
+			// 	d.bmi = 0;
+			// }
+			console.log(+d.bmi * 50);
+			return xScale(+d.bmi * 10);
+		})
+		.attr("cy", function(d) {
+			return yScale(+d.hba1c * 10);
+		})
+		// svg.append("circle")
+		// .attr("r", 5)
+		// .attr("cx", 700)
+		// .attr("cy", 90)
+		.on("mouseover", function(d) {
+			// console.log(d);
+			// console.log("mouseover");
+			// tooltip.style("display", "inline");
+			tooltip.html(getHtmlStringForToolTip(d))
+				.style("left", (d3.event.pageX + 3) + "px")
+				.style("top", (d3.event.pageY + 3) + "px")
+				// .style("display", "inline")
+				.transition()
+				.duration(200) // ms
+				.style("opacity", .9);
+		})
+		.on("mouseout", function() {
+			// console.log("mouseout");
+			// tooltip.style("display", "none")
+			tooltip
+				.transition()
+				.duration(200) // ms
+				.style("opacity", 0);
+		});
 }
